f5c6f1899ba1efa6670ad44b2a084582d07a68ff
wr9263 <wr9263@student.wsb.wroclaw.pl>
2023-06-01 15:18:28 +0200

    Aktualizacja app.py

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
- return '<h1>Hello WSB! Greetings from Flask!</h1>'
+    """
+    Returns a simple HTML greeting.
+    """
+    return '<h1>Hello WSB! Greetings from Flask!</h1>'
 
 if __name__ == "__main__":
- app.run(debug=True)
+    app.run(debug=True)
